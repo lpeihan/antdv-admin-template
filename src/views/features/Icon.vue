1629d608e67d390d2abd2f45a6ecfe625cb5f952
Alfred Li <alfred.li@ringcentral.com>
2025-10-30 10:03:46 +0800

    feat: update

--- src/views/features/Icon.vue
+++ src/views/features/Icon.vue
@@ -1,15 +1,18 @@
 <template>
   <a-card>
-    <div class="flex flex-wrap gap-2">
-      <div
-        v-for="item in svgIconsList"
-        :key="item"
-        class="flex h-[80px] w-[80px] cursor-pointer flex-col items-center justify-center"
-        @click="handleCopy(item)"
+    <div class="flex flex-wrap gap-4">
+      <a-button
+        v-for="svgName in SVG_ICONS_LIST"
+        :key="svgName"
+        type="text"
+        shape="circle"
+        :title="svgName"
+        @click="handleCopy(svgName)"
       >
-        <svg-icon :name="item" class="!size-[24px]" />
-        <span class="pt-2">{{ item }}</span>
-      </div>
+        <template #icon>
+          <svg-icon :name="svgName" class="!size-[18px] align-[-0.3em]" />
+        </template>
+      </a-button>
     </div>
   </a-card>
 </template>
@@ -18,7 +21,7 @@
 import svgIcons from '@/assets/svgIcons';
 import { copyToClipboard } from '@/utils';
 
-const svgIconsList = Object.keys(svgIcons);
+const SVG_ICONS_LIST = Object.keys(svgIcons);
 
 const handleCopy = (item) => {
   copyToClipboard(`<svg-icon name="${item}" />`);
